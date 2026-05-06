3e6cc29819f921fe424024dad4db577e48ae455d
iwawakoiwa <220993652+iwawakoiwa@users.noreply.github.com>
2026-05-06 07:26:04 +0000

    serverで動くように

--- git_start.sh
+++ git_start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+cd /home/iwa/web/mysite
 git pull
 
-docker exec -it mysite bash -c "source ~/.cargo/env && cd /app && dx serve --release --platform web --addr 0.0.0.0"
+docker exec mysite bash -c "pkill -9 dx || true"
+sleep 1
+
+docker exec -d mysite bash -c "source ~/.cargo/env && cd /app && dx serve --platform web --addr 0.0.0.0 --port 8080"
+echo "✅ デプロイ完了"
